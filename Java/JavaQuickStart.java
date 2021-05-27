ab11550911d77c7bd1e6fb9fa887aeab961943a2
Peyman27m <63470748+Peyman27m@users.noreply.github.com>
2021-05-26 21:59:57 -0700

    Update JavaQuickStart.java

--- Java/JavaQuickStart.java
+++ Java/JavaQuickStart.java
@@ -1,7 +1,7 @@
 HttpRequest request = HttpRequest.newBuilder()
 		.uri(URI.create("https://speaker-verification1.p.rapidapi.com/Verification"))
 		.header("content-type", "multipart/form-data; boundary=---011000010111000001101001")
-		.header("x-rapidapi-key", "1c8c1abfe5msh7360a3ec3b0be2fp1b001fjsn7c31862917bf")
+		.header("x-rapidapi-key", "API KEY")
 		.header("x-rapidapi-host", "speaker-verification1.p.rapidapi.com")
 		.method("POST", HttpRequest.BodyPublishers.ofString("-----011000010111000001101001\r
 Content-Disposition: form-data; name=\"sound1\"\r
